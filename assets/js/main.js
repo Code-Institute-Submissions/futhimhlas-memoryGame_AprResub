eaa9a9170bf5913e5a87772550ce5553f135bd37
futhimhlas <futhimhl2@gmail.com>
2021-02-02 17:48:38 +0000

    Main.js update to incldue future sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
+// Makes sure board loads in fully so as to stop users from seeing images too early
+$( document.body ).ready(function() {
+  $("body").fadeIn(2500);
+});
 const tiles = document.querySelectorAll('.tile'); // Generates list of all elements with the class tile and moves into our tiles constant
+
+// Randomizes tile order
+
+// Live update of users score
+
+// Different time out speeds depending on  difficulty chosen
+
+//live score update
+
+// High score tracking for browser
+
+// Stops additional cards from being if animation for first two cards is still running
+
+// Double click protection
+
+
+
+
