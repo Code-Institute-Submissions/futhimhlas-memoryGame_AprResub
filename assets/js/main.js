9b70886ec059e15b5f4ccd269d29be0573351000
futhimhlas <futhimhl2@gmail.com>
2021-04-20 23:27:13 +0000

    JS Update. Shuffle function added. Also tested

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    let tiles = document.querySelectorAll(".tile-wrap");
-    console.log(tiles)
+    let tiles = document.querySelectorAll(".tile-wrap"); // tiles returns all divs with the class tile-wrap as a NodeList which can then be acted upon like an array
+    let gameBoard = $(".tile-board").children() //captures all divs of my gameboard which then allows for them to be shuffled
     
     function flipTiles(){
         this.classList.toggle("flip-tile");
@@ -9,25 +9,17 @@ $(document).ready(function(){
     for(let i = 0; i < tiles.length; i++){
         tiles[i].addEventListener("click", flipTiles)
     }; // Calls flip tile function which then flips each tile as it is clicked
+    
+    function shuffle(a){ // Modified version of fischer yates shuffle. .before method handles moving the elements 
+    let j;
+    for (let i = 0; i < a.length; i++) {
+        j = Math.floor(Math.random() * a.length);
+        $(a[i]).before($(a[j]))
+}
+return this; // function works better with 'this' included
+    };
+shuffle(gameBoard);
 });
-// Generates list of all elements with the class tile and moves into our tiles constant
-
-// Randomizes tile order
-
-// Live update of users score
-
-// Different time out speeds depending on  difficulty chosen
-
-//live score update
-
-// High score tracking for browser
-
-// Stops additional cards from being if animation for first two cards is still running
-
-// Double click protection
-
-// Cards disappearing after being matched
-
 
 
 
