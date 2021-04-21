715e71747011d86956987325bc4cbac90ad05b60
futhimhlas <futhimhl2@gmail.com>
2021-04-21 03:35:57 +0000

    removes unused variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,6 @@ $(document).ready(function(){
     let boardLock = false; //When true, new cards cannot be clicked
     let turns = 0; // Live score tracker
     let endState = 0; //Keeps track of Game progress
-    let moveCount = $("#live-score"); // Container for html element housing scores
     let resetButton = document.getElementById("reset-button") // Container for button housing reset
     
     function flipTiles(){
@@ -29,6 +28,7 @@ $(document).ready(function(){
         tileMatchCheck();
     }
 
+    // tileMatchCheck checks to see if the two flipped cards have the same data attribute (same image)
     function tileMatchCheck(){
         if (firstTile.dataset.image === secondTile.dataset.image){
             removeTile();
@@ -40,6 +40,7 @@ $(document).ready(function(){
         unflipTiles();
     }
 
+    // removeTile() removes the eventListeners from the tile card, removing it from the game
     function removeTile(){
         firstTile.removeEventListener("click", flipTiles);
         secondTile.removeEventListener("click", flipTiles);
@@ -47,6 +48,7 @@ $(document).ready(function(){
         resetGameState();
     }
 
+    // unflipTiles() flips the tiles back over if a match isn't made
     function unflipTiles() {
         boardLock = true;
         setTimeout(function(){
@@ -54,39 +56,41 @@ $(document).ready(function(){
             secondTile.classList.remove("flip-tile");
             resetGameState();
         }, 1000)
-    }
-    // resetGameState allows the player to continue with the game after making a correct match
+    };
+    // resetGameState allows the player to continue with the game after making a choice
     function resetGameState(){
         hasFlippedTile = false;
         boardLock = false;
         firstTile = null;
         secondTile = null;
-    }
+    };
 
     // endStateCheck checks to see if all the pairs have been made. Once all have been made the winning modul pops up
     function endStateCheck(b){
         if(b === 8){
             document.getElementById("modal").style.display="block";
         };
-    }
+    };
 
     // gameReady adds the click event listeners to every tile, allowing for the game to start.
     function gameReady(){
         for(let i = 0; i < tiles.length; i++){
         tiles[i].addEventListener("click", flipTiles); // Calls flip tile function which then flips each tile as it is clicked
     }; 
-}
+};
 
     
     function shuffle(a){ // Modified version of fischer yates shuffle. .before method handles moving the elements 
     let j;
     for (let i = 0; i < a.length; i++) {
         j = Math.floor(Math.random() * a.length);
-        $(a[i]).before($(a[j]))
+        $(a[i]).before($(a[j]));
 }
 return this; // function works better with 'this' included
     };
 shuffle(gameBoard);
+
+// scoreUpdate updates both the final modal and the live score screen
 function scoreUpdate(a){
     document.getElementById("live-score").innerHTML = " Moves: " + a;
     document.getElementById("end-score").innerHTML = a
@@ -97,7 +101,7 @@ resetButton.addEventListener("click", gameReset);
 function gameReset(){
     location.reload();
 }
-gameReady()
+gameReady();
 
 });
 
