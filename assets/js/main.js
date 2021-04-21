9a52d06d4c09e91640a00a3766900bfbb0c169d7
futhimhlas <futhimhl2@gmail.com>
2021-04-21 01:55:31 +0000

    JS Update. Cards now match their partner. Protection also placed incase of choosing 3 cards and double clicking the same card

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,63 @@
 $(document).ready(function(){
     let tiles = document.querySelectorAll(".tile-wrap"); // tiles returns all divs with the class tile-wrap as a NodeList which can then be acted upon like an array
     let gameBoard = $(".tile-board").children() //captures all divs of my gameboard which then allows for them to be shuffled
+
+    let firstTile, secondTile; //Intializes variables to hold our two cards
+    let hasFlippedTile = false;
+    let boardLock = false;
+    let turns = 0;
+    let moveCount = $("#live-score");
     
     function flipTiles(){
-        this.classList.toggle("flip-tile");
+        if (boardLock) return;
+        if(this === firstTile) return;
+        this.classList.add("flip-tile");
+
+        if (!hasFlippedTile){
+            hasFlippedTile = true;
+            firstTile = this;
+            return;
+        }
+
+        secondTile = this;
+
+        tileMatchCheck();
+    }
+
+    function tileMatchCheck(){
+        if (firstTile.dataset.image === secondTile.dataset.image){
+            removeTile();
+            return;
+        }
+
+        unflipTiles();
+    }
+
+    function removeTile(){
+        firstTile.removeEventListener("click", flipTiles);
+        secondTile.removeEventListener("click", flipTiles);
+
+        resetGameState();
+    }
+
+    function unflipTiles() {
+        boardLock = true;
+        setTimeout(function(){
+            firstTile.classList.remove("flip-tile");
+            secondTile.classList.remove("flip-tile");
+            resetGameState();
+        }, 1000)
+    }
+
+    function resetGameState(){
+        hasFlippedTile = false;
+        boardLock = false;
+        firstTile = null;
+        secondTile = null;
     }
 
     for(let i = 0; i < tiles.length; i++){
-        tiles[i].addEventListener("click", flipTiles)
+        tiles[i].addEventListener("click", flipTiles);
     }; // Calls flip tile function which then flips each tile as it is clicked
     
     function shuffle(a){ // Modified version of fischer yates shuffle. .before method handles moving the elements 
@@ -19,6 +69,10 @@ $(document).ready(function(){
 return this; // function works better with 'this' included
     };
 shuffle(gameBoard);
+/*function scoreUpdate(turns){
+    document.getElementById("live-score") = " Score:" + turns;
+    console.log(turns);
+}*/
 });
 
 
